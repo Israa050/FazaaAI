ffc88934409033c925116dafcc48667f18fed397
Fatimakhafaji <151563981+Fatimakhafaji@users.noreply.github.com>
2025-03-16 17:47:15 +0300

    Add files via upload
    with fazaa ai

--- lib/features/auth/screens/login.dart
+++ lib/features/auth/screens/login.dart
@@ -1,84 +1,86 @@
 import 'package:flutter/material.dart';
-import '../../../core/themes/colors.dart';
-
+import 'package:get/get.dart';
+//import 'package:salam/models/users.dart';
+import 'package:salam/views/homepage.dart';
+import 'package:salam/views/Sign_up.dart';
 
 class Login extends StatelessWidget {
-  const Login({super.key});
-
+  Login({super.key});
+  //Users user = Users(, '', password, email, role, phonenumber, address)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.background,
-     // const Color.fromARGB(255, 250, 243, 243),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/login.png',
-                  height: 390,
-                  width: 350,
+      backgroundColor: Colors.white,
+      body: Column(children: [
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'images/FazaaAI.png',
+                height: 390,
+                width: 350,
+              ),
+              const Text(
+                'Welcome Back',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              const SizedBox(height: 25.0),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'user name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
                 ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 50,
+                width: 320,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.blue,
+                  onPressed: () {
+                    Get.to(Homepage());
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 const Text(
-                  'Welcome Back',
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black38),
+                  'Dont have an account?',
+                  style: TextStyle(color: Colors.black),
                 ),
-                const SizedBox(height: 25.0),
-                const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  height: 50,
-                  width: 320,
-                  child: FloatingActionButton(
-                    backgroundColor: AppColors.primaryBlue,
+                TextButton(
                     onPressed: () {
-                      //Get.to(Homepage());
+                      //if(){},
+                      Get.to(SignUp());
                     },
                     child: const Text(
-                      'Login',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  const Text(
-                    'Dont have an account?',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        //Get.to(SignUp());
-                      },
-                      child: const Text(
-                        'Sign up',
-                        style: TextStyle(color: AppColors.darkBlue,),
-                      ))
-                ])
-              ],
-            ),
+                      'Sign up',
+                      style: TextStyle(color: Colors.blue),
+                    ))
+              ])
+            ],
           ),
         ),
-      ),
+      ]),
     );
   }
 }
